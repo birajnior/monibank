9b2676f9610568a6bab075f3a6c9f6e48cbd1b2c
birajnior <ubiratanbernardo@gmail.com>
2023-11-28 18:45:16 -0300

    add script dee reconhecimento facial

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,23 @@ import ehUmCPF from "./valida-cpf.js";
 import ehMaiorDeIdade from "./valida-idade.js";
 
 const camposDoFormulario = document.querySelectorAll("[required]");
+const formulario = document.querySelector("[data-formulario");
+
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const listaRespostas = {
+    "nome": e.target.elements["nome"].value,
+    "email": e.target.elements["email"].value,
+    "rg": e.target.elements["rg"].value,
+    "cpf": e.target.elements["cpf"].value,
+    "aniversario": e.target.elements["aniversario"].value,
+  };
+
+  localStorage.setItem("cadastro", JSON.stringify(listaRespostas))
+
+  window.location.href = '../pages/abrir-conta-form-2.html'
+});
 
 camposDoFormulario.forEach((campo) => {
   campo.addEventListener("blur", () => verificaCampo(campo));
@@ -49,7 +66,7 @@ const mensagens = {
 
 function verificaCampo(campo) {
   let mensagem = "";
-  campo.setCustomValidity('')
+  campo.setCustomValidity("");
   if (campo.name == "cpf" && campo.value.length >= 11) {
     ehUmCPF(campo);
   }
